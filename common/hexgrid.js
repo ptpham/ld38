afa57b1a3f578171cc5449252aa4022947d3a4b7
Peter Pham <ptr.pham@gmail.com>
2017-04-21 23:22:16 -0700

    Adding hexgrid lookup.

--- common/hexgrid.js
+++ common/hexgrid.js
@@ -6,6 +6,9 @@ var shifts = [[1,0],[-1,0],[0,1],[0,-1],[1,-1],[-1,1]]
 
 var _v2_0 = vec2.create();
 var rot60 = mat2.fromRotation(mat2.create(), Math.PI/3);
+var sec30 = 1/Math.cos(Math.PI/6);
+var tan30 = Math.tan(Math.PI/6);
+var _adjacents = shifts.map(x => vec2.create());
 
 export class HexGrid {
   constructor(scale, di) {
@@ -16,6 +19,27 @@ export class HexGrid {
 
   center(result, i, j) {
     return HexGrid.center(result, this.scale, i, j, this.di, this.dj);
+  }
+
+  lookup(result, point) {
+    return HexGrid.lookup(result, this.scale, point, this.di, this.dj);
+  }
+
+  static lookup(result, scale, point, di, dj) {
+    var pi = vec2.dot(di, point)/scale;
+    var pio = Math.sqrt(vec2.lengthSquared(point) - pi*pi)/scale;
+    var iamt = pi - sec30*pio;
+    var jamt = tan30*pio;
+    
+    var it = Math.floor(iamt + 0.5);
+    var jt = Math.floor(jamt + 0.5);
+
+    HexGrid.adjacent(_adjacents, it, jt);
+    for (var adj of _adjacents) {
+      HexGrid.center(adj, scale, adj[0], adj[1], di, dj);
+    }
+    return vec2.copy(result,
+      _.minBy(_adjacents, center => vec2.distance(center, point)));
   }
 
   static rotate(result, di) {
