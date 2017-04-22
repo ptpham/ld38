794d34a68931d82b340abcc8a97afe3d8d404892
Peter Pham <ptr.pham@gmail.com>
2017-04-21 21:50:20 -0700

    Adding hex grid adjacents.

---
+++ common/hexgrid.js
@@ -0,0 +1,14 @@
+
+import { vec2 } from 'gl-matrix';
+var shifts = [[1,0],[-1,0],[0,1],[0,-1],[1,-1],[-1,1]]
+  .map(x => vec2.fromValues(x[0], x[1]));
+
+var _v2_0 = vec2.create();
+
+export class HexGrid {
+  static adjacent(i, j) {
+    vec2.set(_v2_0, i, j);
+    return shifts.map(x => vec2.add(vec2.clone(x), _v2_0, _v2_0));
+  }
+}
+
