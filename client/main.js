3907361b613a120e7a67e77f00fbb6362fbe984a
Cindy Zhang <cixzhang@gmail.com>
2017-04-22 09:35:08 -0700

    tiles use mongo collections

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 
 import './main.html';
 import { Meteor } from 'meteor/meteor';
+import Tiles from '../common/tiles';
 
 Meteor.startup(() => {
+  Meteor.subscribe('tiles');
 
+  console.log(Tiles.find().fetch());
 });
-
