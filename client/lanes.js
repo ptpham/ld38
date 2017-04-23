a97cf44862229a118ca34e9af4fb94bf6688a436
Peter Pham <ptr.pham@gmail.com>
2017-04-23 11:12:57 -0700

    Car now faces the right direction all the time.

--- client/lanes.js
+++ client/lanes.js
@@ -5,18 +5,19 @@ import _ from 'lodash';
 
 var _v3_0 = vec3.create();
 
-var SHIFT_Y = 0.7;
-var SHIFT_X = 0.8;
+var SHIFT_Y = -0.7;
+var SHIFT_X = 2;
 
 export default class Lanes {
   static applyLaneTransform(result, orientation, leaving, carScale) {
     mat4.rotateZ(result, result, orientation*Math.PI/3);
 
-    vec3.set(_v3_0, carScale, carScale, carScale);
+    var sign = (leaving ? 1 : -1);
+    vec3.set(_v3_0, sign*carScale, sign*carScale, carScale);
     mat4.scale(result, result, _v3_0);
 
-    _v3_0[0] = SHIFT_X;
-    _v3_0[1] = (leaving ? 1 : -1)*SHIFT_Y;
+    _v3_0[0] = leaving ? SHIFT_X : -SHIFT_X - 0.5;
+    _v3_0[1] = SHIFT_Y;
     mat4.translate(result, result, _v3_0);
     return result;
   }
